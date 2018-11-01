e0329b5636887e487794b5a4d053d5aa8094079e
tsijercic1 <tsijercic1@etf.unsa.ba>
2018-11-01 23:05:13 +0100

    Implemented intersect(), isNull(), changed private values from primitive to wrapper

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,8 +1,10 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
-    private double left, right;
-    private boolean leftInclude,rightInclude;
+    private Double left, right;
+    private Boolean leftInclude,rightInclude;
+
+    public Interval() { }
 
     public Interval(double left, double right, boolean leftInclude, boolean rightInclude) {
         this.left = left;
@@ -51,17 +53,45 @@ public class Interval {
             i=i2;
             i2=temp;
         }
-        if(Double.compare(i2.getLeft(),i.getLeft())<=0 &&
-                Double.compare(i.getRight(),i2.getRight())<=0 ){
+        temp = new Interval(0,0,false,false);
+        if(Double.compare(i.getLeft(),i2.getLeft())<=0 && Double.compare(i2.getRight(),i.getRight())<=0 ){
+            temp.setLeftInclude(i2.isLeftInclude());
+            temp.setRightInclude(i2.isRightInclude());
+            temp.setRight(i2.right);
+            temp.setLeft(i2.left);
 
+        }else if(Double.compare(i.getRight(),i2.getRight())<0 && Double.compare(i.getLeft(),i2.getRight())<0){
+                temp.setLeft(i2.getLeft());
+                temp.setRight(i.getRight());
+                temp.setLeftInclude(i2.isLeftInclude());
+                temp.setRightInclude(i.isRightInclude());
         }
+        if(Double.compare(i2.getLeft(), i.getLeft()) == 0){
+            temp.setLeftInclude(i.leftInclude && i2.leftInclude);
+        }
+        if( Double.compare(i.getRight(),i2.getRight())==0 ){
+            temp.setRightInclude(i.rightInclude && i2.rightInclude);
+        }
+
+        return temp;
     }
 
+    public Interval intersect(Interval i)
+    {
+        Interval temp=intersect(this, i);
+        this.left=temp.left;
+        this.right=temp.right;
+        this.leftInclude = temp.leftInclude;
+        this.rightInclude = temp.rightInclude;
+        return this;
+    }
 
     public boolean isIn(double v) {
-
+        return (left<v&&v<right)||(Double.compare(v,left)==0 && leftInclude)||(Double.compare(v,right)==0 && rightInclude);
     }
 
     public boolean isNull() {
+        if(left==null)return true;
+        return false;
     }
 }
